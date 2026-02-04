25bf7c9110358439770c5dbb7f2c826214eb7ece
Lance Ward <93290915+Thoriangel321@users.noreply.github.com>
2026-02-04 20:17:58 +0200

    added files

---
+++ Lab1/CS/Lab1/Program.cs
@@ -0,0 +1,29 @@
+﻿using Lab1;
+using System;
+
+namespace Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //----------------------
+            Square square1 = new Square();
+            List<double> points = new List<double> {2, 6, 8, 6, 2, 1, 8, 1};
+            square1.AddPoints(points);
+            //----------------------
+
+            System.Console.WriteLine("square's coordinates are: ");
+            for (int i = 0; i <= 7; i++) 
+            {
+                Console.Write(points[i] + " ");
+                if(i % 2 == 1) 
+                {
+                    Console.WriteLine("");
+                }
+            }
+
+            Console.WriteLine("square is - " + square1.square() + " perimeter is - " + square1.perimeter());
+        }
+    }
+}
---
+++ Lab1/CS/Lab1/Square.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab1
+{
+    internal class Square
+    {
+        
+        private List<double> points;
+
+        public Square() 
+        {
+            points = new List<double>();
+        }
+
+        public void AddPoints(IEnumerable<double> morepoints) 
+        {
+            points.AddRange(morepoints);
+        }
+
+        public double square()
+        {
+            double length = System.Math.Sqrt(System.Math.Pow((points[2] - points[0]), 2) + System.Math.Pow((points[3] - points[1]), 2));
+            return (length * length);
+        }
+        public double perimeter()
+        {
+            double length = System.Math.Sqrt(System.Math.Pow((points[2] - points[0]), 2) + System.Math.Pow((points[3] - points[1]), 2));
+            return 4 * length;
+        }
+    }
+}
